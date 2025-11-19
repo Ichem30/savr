4eb6984db806ef9b004c45354e9685b9398dc1b9
Ichem <ichem30@gmail.com>
2025-11-19 22:17:41 +0100

    Setup Capacitor for iOS and Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pantrychef.app',
+  appName: 'PantryChef',
+  webDir: 'dist'
+};
+
+export default config;
